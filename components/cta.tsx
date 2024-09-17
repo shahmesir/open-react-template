d4751da67980cd95bd4c00ca3dbf5feca14cc01d
Gillan (Jitera) <developers@jitera.com>
2024-09-17 11:15:07 +0000

    feat: update CTA color using Tailwind CSS

--- components/cta.tsx
+++ components/cta.tsx
@@ -28,7 +28,7 @@ export default function Cta() {
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
               <div data-aos="fade-up" data-aos-delay={400}>
                 <a
-                  className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
+                  className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto cta-button"
                   href="#0"
                 >
                   <span className="relative inline-flex items-center">
@@ -52,5 +52,5 @@ export default function Cta() {
         </div>
       </div>
     </section>
-  );
+  )
 }
